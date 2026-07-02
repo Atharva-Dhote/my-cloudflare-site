7f54cd47a93c1f139a9807e832134e81046755b9
Atharva-Dhote <143984230+Atharva-Dhote@users.noreply.github.com>
2026-07-02 10:11:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+for(let i=0;i<60;i++){
 
+const star=document.createElement("div");
+
+star.className="star";
+
+star.style.left=Math.random()*100+"vw";
+
+star.style.top=Math.random()*100+"vh";
+
+star.style.animationDuration=5+Math.random()*10+"s";
+
+star.style.animationDelay=Math.random()*5+"s";
+
+document.body.appendChild(star);
+
+}
